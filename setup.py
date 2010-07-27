f6d61a9a96df9717bca661d4d58ef19376d8cc5a
Nick Welch <nick@incise.org>
2010-07-26 22:59:43 -0700

    Initial attempt at packaging.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+setup(
+    name = "HTPicker",
+    version = "0.1alpha",
+    packages = find_packages(),
+    scripts = ['htpicker'],
+
+    install_requires = ['distribute'],
+
+    package_data = {
+        'htpicker': [
+            'data/js/*.js',
+            'data/css/*/*.css',
+            'data/css/*/images/*'
+            'data/images/*/*',
+            'data/app.html',
+        ],
+    },
+
+    entry_points = {
+        'console_scripts': ['htpicker = htpicker:main'],
+    },
+
+    author = "Nick Welch",
+    author_email = "nick@incise.org",
+    description = "A simple home theater frontend",
+    license = "Public Domain + CC Attribution + LGPL + MIT",
+    keywords = "home theater htpc frontend media player",
+    url = "http://github.com/mackstann/htpicker",
+)

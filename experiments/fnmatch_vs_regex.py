50595f8c0fc1872a6fd9e749d1db6892f41841d8
Nick Welch <nick@incise.org>
2010-10-18 23:06:35 -0700

    Fixed flawed comparison wrt filename/pattern case.
    Comparison now assumes that we can lower() both the filename and the
    regex/glob before comparing them.  This is necessary because Python's
    fnmatch (unlike real libc fnmatch) has no case-insensitive option.
    
    Also, re.I is slow.  Much slower than doing "[aA][bB]..", surprisingly.
    
    A single regex is now the performance winner, and is probably best in
    other ways anyway: flexibility, and freedom from the fnmatch module's
    quirks.

--- experiments/fnmatch_vs_regex.py
+++ experiments/fnmatch_vs_regex.py
@@ -8,41 +8,45 @@ path = sys.argv[1]
 num_iterations = int(sys.argv[2])
 
 fnmatch_patterns = "*~ *.bak *.nfo *.txt *.url *.sfv *.part*.rar".split()
-regex = re.compile(r'.*(?:~|\.bak|\.nfo|\.txt|\.url|\.sfv|\.part.*\.rar)$', re.I)
+regex = re.compile(r'.*(?:~|\.bak|\.nfo|\.txt|\.url|\.sfv|\.part.*\.rar)$')
 separate_regexes = (
-    re.compile(r'.*~$', re.I),
-    re.compile(r'.*\.bak$', re.I),
-    re.compile(r'.*\.nfo$', re.I),
-    re.compile(r'.*\.txt$', re.I),
-    re.compile(r'.*\.url$', re.I),
-    re.compile(r'.*\.sfv$', re.I),
-    re.compile(r'.*\.part.*\.rar$', re.I),
+    re.compile(r'.*~$'),
+    re.compile(r'.*\.bak$'),
+    re.compile(r'.*\.nfo$'),
+    re.compile(r'.*\.txt$'),
+    re.compile(r'.*\.url$'),
+    re.compile(r'.*\.sfv$'),
+    re.compile(r'.*\.part.*\.rar$'),
 )
 
 proc = subprocess.Popen("find {0}".format(pipes.quote(path)),
         shell=True, stdout=subprocess.PIPE)
 stdout, stderr = proc.communicate()
 
-lines = stdout.splitlines()
+lines = map(str.lower, stdout.splitlines())
 num_files = len(lines)
 
 def test_fnmatch():
+    fn = fnmatch.fnmatchcase
     for filename in lines:
         for pattern in fnmatch_patterns:
-            fnmatch.fnmatch(filename, pattern)
+            fn(filename, pattern)
 
 def test_fnmatch_filter():
+    fn = fnmatch.filter
     for pattern in fnmatch_patterns:
-        fnmatch.filter(lines, pattern)
+        fn(lines, pattern)
 
 def test_regex():
+    fn = regex.match
     for filename in lines:
-        regex.match(filename)
+        fn(filename)
 
 def test_separate_regexes():
-    for filename in lines:
-        for regex in separate_regexes:
-            regex.match(filename)
+    for regex in separate_regexes:
+        fn = regex.match
+        for filename in lines:
+            fn(filename)
 
 t = timeit.Timer(stmt=test_fnmatch)
 print "fnmatch: %.2f usec per file" % (1000000 * t.timeit(number=num_iterations)/num_iterations/num_files)
@@ -57,7 +61,7 @@ t = timeit.Timer(stmt=test_separate_regexes)
 print "separate regexes: %.2f usec per file" % (1000000 * t.timeit(number=num_iterations)/num_iterations/num_files)
 
 # and the results are...
-# fnmatch: 19.68 usec per file
-# fnmatch filter: 4.75 usec per file
-# regex: 13.68 usec per file
-# separate regexes: 16.95 usec per file
+# fnmatch: 8.15 usec per file
+# fnmatch filter: 5.61 usec per file
+# regex: 3.62 usec per file
+# separate regexes: 5.45 usec per file

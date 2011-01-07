edb1866a4639c202525cbc297de2d283ff2ab914
Nick Welch <nick@incise.org>
2011-01-07 00:04:14 -0800

    Bumping version to 0.1 in setup.py.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "htpicker",
-    version = "0.1alpha",
+    version = "0.1",
     packages = find_packages(),
 
     install_requires = ['distribute'],

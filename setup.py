f15ddb0fe4d0b27a6f1563f4beaefb41ac38d6f1
Nick Welch <nick@parthenonsoftware.com>
2011-01-07 10:20:32 -0800

    Okay, ready for 0.1.1.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "htpicker",
-    version = "0.1",
+    version = "0.1.1",
     packages = find_packages(),
 
     install_requires = ['distribute'],

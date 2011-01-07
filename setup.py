1b7e72d5aa9646a0177a65cdde0753a5939f163d
Nick Welch <nick@parthenonsoftware.com>
2011-01-07 10:39:24 -0800

    Getting close... trying 0.1.3 now.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "htpicker",
-    version = "0.1.2",
+    version = "0.1.3",
     packages = find_packages(),
 
     install_requires = ['distribute'],
